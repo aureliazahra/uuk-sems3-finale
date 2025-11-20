6ad144feb61c5d6a116f6d625f85aa71e3eee20c
Aurelia <aurelaurelzr@gmail.com>
2025-11-20 21:42:45 +0700

    add article all

---
+++ lib/widgets/grid_artikel_all.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../../models/artikel_model.dart';
+
+class GridArtikelAll extends StatelessWidget {
+  final List<Blog> artikelList;
+  const GridArtikelAll({super.key, required this.artikelList});
+
+  @override
+  Widget build(BuildContext context) {
+    const basedUrl = 'https://api-pariwisata-rakryan.id';
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        childAspectRatio: 1,
+      ),
+      itemCount: artikelList.length,
+      itemBuilder: (context, index) {
+        final artikel = artikelList[index];
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Gambar artikel
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage('$basedUrl/${artikel.image}'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            //end gambar artikel
+            //judul artikel
+            Text(
+              artikel.title,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            //end judul artikel
+            //deskripsi artikel
+            Text(
+              artikel.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 12),
+            ),
+            //end deskripsi artikel
+          ],
+        );
+      },
+    );
+  }
+}
